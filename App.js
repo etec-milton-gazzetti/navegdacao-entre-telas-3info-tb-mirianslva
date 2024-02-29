36c4e0efc98c07936b416eaeba05a1185037c876
mirianslva <142011303+mirianslva@users.noreply.github.com>
2024-02-29 12:50:33 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela Inicial</Text>
+      <Button title='Sobre' onPress={()=> navigation.navigate("Sobre")}/> 
+          </View>
+  );
+}
+function Sobre({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sobre</Text>
+      <Button title='Voltar' onPress={()=> navigation.goBack()}/> 
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Sobre" component={Sobre} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
